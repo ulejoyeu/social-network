b37ac3e7cf03937fc8748361146c3a65ceeb7fad
ulejoyeu <krilius974@gmail.com>
2022-06-16 22:54:41 +0200

    reorganize folders

--- client/src/components/home/home-sidebar.js
+++ client/src/components/home/home-sidebar.js
@@ -3,6 +3,10 @@ import React from 'react';
 const HomeSidebar = () => {
     return (
         <div className="w-1/4 bg-slate-200 min-h-full border-slate-200 border-r shadow-lg">
+            <ul className="p-4">
+                <li className="text-xl mt-4">Mon profil</li>
+                <li className="text-xl mt-4">Mes groupes</li>
+            </ul>
         </div>
     )
 };
